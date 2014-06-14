d0afc982c91c4f10ffc92c57517a4f04c56280c0
RailsCarma <github@carmatec.com>
2014-06-14 15:09:06 +0530

    Update add_minimum_order_to_groups.rb

--- app/overrides/add_minimum_order_to_groups.rb
+++ app/overrides/add_minimum_order_to_groups.rb
@@ -3,7 +3,7 @@ path="spree/admin/user_groups"
 Deface::Override.new(:virtual_path=> "#{path}/index",
   :name=> "minimum_order_column_header",
   :insert_before => "th[3]",
-  :text=> "<th width='0px'>Minimum Order</th>",
+  :text=> "<th width='0px'>Minimum Order Amount</th>",
   :original=> "a8447ceffcd5c4085af882f72f0e2f322c442726")
 
 Deface::Override.new(:virtual_path=> "#{path}/index",
@@ -14,4 +14,4 @@ Deface::Override.new(:virtual_path=> "#{path}/index",
 Deface::Override.new(:virtual_path=> "#{path}/_form",
   :name=> "minimum_order_edit_field",
   :insert_after=> "erb[loud]:contains('f.text_field :name')",
-  :text=> "<br /> <%= f.label :minimum_order %> <br /> <%= f.text_field :minimum_order %>")
+  :text=> "<br /> <%= f.label :minimum_order_amount %> <br /> <%= f.text_field :minimum_order %>")
